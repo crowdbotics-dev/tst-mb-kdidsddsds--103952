44e7e68b034972d7ef47f819b55f7cdbe5baea8f
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-21 14:33:11 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -2,4 +2,7 @@ from django.conf import settings
 from django.db import models
 class Hello(models.Model):
     'Generated Model'
-    sdsdsdsdssd = models.BigIntegerField(blank=True,null=True,)
+    sdsdsdsdssd = models.BigIntegerField(null=True,blank=True,)
+class Helloo(models.Model):
+    'Generated Model'
+    dsdsds = models.BigIntegerField()
