659df38bdeab5d8dbf6512f48acb9e8947cf19c3
Crowdbotics <engineering@crowdbotics.com>
2023-08-24 16:13:45 +0000

    Update store.

--- store/tstmbkdidsddsdsAPI/helloos.slice.js
+++ store/tstmbkdidsddsdsAPI/helloos.slice.js
@@ -47,118 +47,119 @@ const helloosSlice = createSlice({
   name: "helloos",
   initialState,
   reducers: {},
-  extraReducers: {
-    [api_v1_helloo_list.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [api_v1_helloo_list.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = action.payload
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_list.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_create.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [api_v1_helloo_create.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities.push(action.payload)
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_create.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_retrieve.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [api_v1_helloo_retrieve.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = [
-          ...state.entities.filter(record => record.id !== action.payload.id),
-          action.payload
-        ]
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_retrieve.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_update.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [api_v1_helloo_update.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = state.entities.map(record =>
-          record.id === action.payload.id ? action.payload : record
-        )
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_update.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_partial_update.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [api_v1_helloo_partial_update.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = state.entities.map(record =>
-          record.id === action.payload.id ? action.payload : record
-        )
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_partial_update.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_destroy.pending]: (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending"
-      }
-    },
-    [api_v1_helloo_destroy.fulfilled]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = state.entities.filter(
-          record => record.id !== action.meta.arg?.id
-        )
-        state.api.loading = "idle"
-      }
-    },
-    [api_v1_helloo_destroy.rejected]: (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
-      }
-    }
+  extraReducers: builder => {
+    builder
+      .addCase(api_v1_helloo_list.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_helloo_list.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = action.payload
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_list.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_create.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_helloo_create.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities.push(action.payload)
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_create.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_retrieve.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_helloo_retrieve.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = [
+            ...state.entities.filter(record => record.id !== action.payload.id),
+            action.payload
+          ]
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_retrieve.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_update.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_helloo_update.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = state.entities.map(record =>
+            record.id === action.payload.id ? action.payload : record
+          )
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_update.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_partial_update.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_helloo_partial_update.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = state.entities.map(record =>
+            record.id === action.payload.id ? action.payload : record
+          )
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_partial_update.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_destroy.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_helloo_destroy.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = state.entities.filter(
+            record => record.id !== action.meta.arg?.id
+          )
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_helloo_destroy.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
   }
 })
 export default {
